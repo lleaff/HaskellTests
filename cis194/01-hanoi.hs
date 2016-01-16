c17fd77c0ad9e81fa049942c86eccd13e18733a7
lleaff <lleaff@users.noreply.github.com>
2016-01-16 18:02:40 +0100

    Solve

--- cis194/01-hanoi.hs
+++ cis194/01-hanoi.hs
@@ -1,11 +1,14 @@
-module Hanoi where
-
 type Peg = String
 type Move = (Peg, Peg)
 
 move :: Peg -> Peg -> Move
 move a b = (a, b)
 
-
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n a b c = ''
+hanoi n a b c
+  | n == 1    = [move a b]
+  | otherwise = hanoi (n - 1) a c b ++ [move a b] ++ hanoi (n - 1) c b a
+
+main :: IO()
+main = print (hanoi 2 "a" "b" "c") >>
+       print (hanoi 3 "a" "b" "c")
