2dccb0e85cf78f2f55f3e3a2b2fa5f62be7fe2ce
lleaff <lleaff@users.noreply.github.com>
2016-01-18 22:28:00 +0100

    set operator precedence

--- realWorldHaskell/02.hs
+++ realWorldHaskell/02.hs
@@ -3,6 +3,11 @@ module Chapter02 where
 (|||) :: Bool -> Bool -> Bool
 True ||| _  = True
 False ||| b = b
+-- https://www.haskell.org/onlinereport/decls.html#fixity
+infixr 2 |||
+
+(|>) :: a -> (a -> b) -> b
+a |> f = f a
 
 mylength :: [a] -> Integer
 mylength (_:xs) = 1 + mylength xs
@@ -13,10 +18,10 @@ mynull [] = True
 mynull _  = False
 
 
-{- mydrop :: Integral a => a -> [a] -> [a]
-mydrop n ls = if n <= 0 ||| mynull ls
+mydrop' :: Integral a => a -> [a] -> [a]
+mydrop' n ls = if n <= 0 ||| mynull ls
               then ls
-              else mydrop (n - 1) (tail ls) -}
+              else mydrop' (n - 1) (tail ls)
 
 mydrop :: Integral b => b -> [a] -> [a]
 mydrop n (x:xs)
@@ -26,3 +31,13 @@ mydrop _ [] = []
 
 (%) :: Integral a => a -> a -> a
 x % y = x - ((x `div` y) * y)
+
+lastButOne :: [a] -> a
+lastButOne [x, _] = x
+lastButOne (_:xs) = lastButOne xs
+lastButOne _      = error "Chapter02.lastButOne: list is too short (2 minimum)"
+
+safeLast :: [a] -> Maybe a
+safeLast []     = Nothing
+safeLast (x:[]) = Just x
+safeLast (_:xs) = safeLast xs
