109daaec6fcb8fa8c79f9465cbf2cec5279826c0
lleaff <lleaff@users.noreply.github.com>
2016-01-24 02:24:07 +0100

    snails

--- src/babyType.hs
+++ src/babyType.hs
@@ -1,25 +1,76 @@
 module BabyType where
 
+import Data.Char
+
+------------------------------------------------------------
+
 type Name = String
 type Head = Char
 type Body = Char
 
 data Snailnimal = Snail Name Head Body
+                | BabySnail Name Head
                 | DeadSnail Name
+
+getName :: Snailnimal -> Name
+getName (Snail name _ _) = name
+getName (BabySnail name _) = name
+getName (DeadSnail name) = name
 
 instance Show Snailnimal where
   show (Snail name shead body) =
             show ([body, shead] ++ " <= " ++ name ++ " the snail")
+  show (BabySnail name shead) =
+            show ([shead] ++ " <= " ++ name ++ " the baby snail")
   show (DeadSnail name) =
             show ("+ <= Late " ++ name ++ " the snail")
     -- ✝
 
-kill :: Snailnimal -> Snailnimal
-kill (Snail name _ _) = DeadSnail name
-kill dead = dead 
+match :: Snailnimal -> Snailnimal -> Snailnimal
+match (Snail name shead _) (Snail name' shead' _') =
+    BabySnail (matchNames name name') (matchHeads shead shead')
+match _ _ = error "match: Can't do that"
 
-charlie :: Snailnimal
+grow :: Snailnimal -> Snailnimal
+grow (BabySnail name shead) = Snail name shead (middleChar 'A' shead)
+grow (Snail name _ _) = DeadSnail name
+grow (DeadSnail _) = error "grow: Can't do that"
+
+kill :: Snailnimal -> Snailnimal
+kill snail = DeadSnail (getName snail)
+
+------------------------------------------------------------
+
+middle :: Integral a => a -> a -> a
+middle x = (+ x) . (`div` 2) . (-x +)
+
+middleChar :: Char -> Char -> Char
+middleChar h = chr . middle (ord h) . ord
+
+pair :: a -> a -> [a]
+pair x y = [x, y]
+
+weave :: [a] -> [a] -> [a]
+weave xs ys = concat (zipWith pair xs ys)
+
+compress :: [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:zs) = x : compress (tail zs)
+
+capitalize :: String -> String
+capitalize (c:cs) = toUpper c : map toLower cs
+capitalize "" = ""
+
+matchNames :: String -> String -> String
+matchNames x y = capitalize (weave (compress x) (reverse (compress y)))
+
+matchHeads = middleChar
+
+------------------------------------------------------------
+
 charlie = Snail "Charlie" 'o' '@'
 
-bobby :: Snailnimal
+julie = Snail "Julie" 'u' 'O'
+
 bobby = DeadSnail "Bobby"
