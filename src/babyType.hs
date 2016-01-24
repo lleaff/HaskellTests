cd15f6c1b7f10b3dbe6baf081f5086ec144e8875
lleaff <lleaff@users.noreply.github.com>
2016-01-24 01:18:58 +0100

    Algebraic data type

--- src/babyType.hs
+++ src/babyType.hs
@@ -5,10 +5,21 @@ type Head = Char
 type Body = Char
 
 data Snailnimal = Snail Name Head Body
+                | DeadSnail Name
 
 instance Show Snailnimal where
   show (Snail name shead body) =
-    show ([body, shead] ++ " <= " ++ name ++ " the snail")
+            show ([body, shead] ++ " <= " ++ name ++ " the snail")
+  show (DeadSnail name) =
+            show ("+ <= Late " ++ name ++ " the snail")
+    -- ✝
+
+kill :: Snailnimal -> Snailnimal
+kill (Snail name _ _) = DeadSnail name
+kill dead = dead 
 
 charlie :: Snailnimal
 charlie = Snail "Charlie" 'o' '@'
+
+bobby :: Snailnimal
+bobby = DeadSnail "Bobby"
